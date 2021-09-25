8d26b200d20e7fb4954a0356f28e1031d9bc84ea
Akash <akkida746@gmail.com>
2021-09-25 15:56:23 +0530

    Added reference url, code refactor is required

--- src/main/java/com/akash/Application.java
+++ src/main/java/com/akash/Application.java
@@ -3,6 +3,8 @@ package com.akash;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// References:
+// https://github.com/javadevjournal/javadevjournal/tree/master/Spring-Boot/multiple-data-sources
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
